f8bb0aacb8779faea6eb214f06a480a855168755
Xyntax <cdxy000@gmail.com>
2021-07-29 19:56:47 +0800

    init

---
+++ robot/conf.py
@@ -0,0 +1,60 @@
+# -*- encoding: utf-8 -*-
+webhook_url = 'https://open.feishu.cn/open-apis/bot/v2/hook/004c43f9-24ef-49b1-a5b2-44683c5bf4e9'
+webhook_template = '''
+{
+	"msg_type": "post",
+	"content": {
+		"post": {
+			"zh_cn": {
+				"title": "$BLOG_NAME",
+				"content": [
+					[
+						{
+							"tag": "a",
+							"text": "$TITLE_TEXT",
+							"href": "$LINK"
+						}
+					]
+				]
+			}
+		}
+	}
+}  
+'''
+
+# webhook_template = '''
+# {
+# 	"msg_type": "interactive",
+# 	"update_multi": false,
+# 	"card": {
+# 		"config": {
+# 			"wide_screen_mode": true
+# 		},
+# 		"header": {
+# 			"title": {
+# 				"tag": "plain_text",
+# 				"content": "$BLOG_NAME - $TITLE_TEXT"
+# 			}
+# 		},
+# 		"elements": [{
+# 			"tag": "div",
+# 			"text": {
+# 				"tag": "plain_text",
+# 				"content": "$SUMMARY"
+# 			}
+# 		},
+# 		{
+# 			"tag": "action",
+# 			"actions": [{
+# 				"tag": "button",
+# 				"text": {
+# 					"tag": "plain_text",
+# 					"content": "Read"
+# 				},
+# 				"url": "$LINK",
+# 				"type": "default"
+# 			}]
+# 		}]
+# 	}
+# }
+# '''
--- robot/rss.py
+++ robot/rss.py
@@ -7,18 +7,35 @@ rss.py
 推送 Rss 信息到钉钉机器人Api
  
 """
+
 from datetime import datetime
 import feedparser
-from dingtalkchatbot.chatbot import DingtalkChatbot, CardItem, ActionCard
 import dateparser
 from models import db, Rss, History
+from conf import webhook_url, webhook_template
 import os
+import requests
+import re
+import json
+
+
+# h = {
+#     'Content-Type': 'text/json'
+# }
+
+class CardItem:
+    def __init__(self, title, url, pic_url, blog_name, summary=''):
+        self.title = title
+        self.url = url
+        self.pic_url = pic_url
+        self.summary = summary
+        self.blog_name = blog_name
+
 
 class RssRobot:
     def __init__(self):
-        self.robot = DingtalkChatbot(
-            os.environ.get("DD_WEBHOOK"),
-            pc_slide=True, secret=os.environ.get("DD_SECRET"))
+        self.webhook = webhook_url
+        self.card = webhook_template
 
     def parse_rss(self):
         # self.remove_old_history()
@@ -28,21 +45,29 @@ class RssRobot:
                          History.select().where(History.publish_at == datetime.today().strftime("%Y-%m-%d"))]
         for rss in rss_list:
             rss_history_list = []
-            card_list = [
-                CardItem(title=rss.title, url=rss.url, pic_url=rss.cover)]
-            feed = feedparser.parse(rss.feed)            
+            card_list = []
+            feed = feedparser.parse(rss.feed)
+            print(feed.entries)
             for entry in feed.entries:
-                if entry.link not in post_url_list and self.is_today(entry):
-                    card_list.append(CardItem(title=f'{entry.title}', url=entry.link,
-                                            pic_url='https://ftp.bmp.ovh/imgs/2020/07/6cdb9f606677c9e3.jpg'))
+                if entry.link not in post_url_list:
+                    card_list.append(CardItem(title=f'{entry.title}',
+                                              url=entry.link,
+                                              pic_url='https://ftp.bmp.ovh/imgs/2020/07/6cdb9f606677c9e3.jpg',
+                                              blog_name=rss.title,
+                                              summary=self.parse_summary(entry.summary)))
                     rss_history_list.append(History(url=entry.link))
-
-            if len(card_list) > 1:
+            print('{} new post found in feed:{}'.format(len(card_list), rss.url))
+            if len(card_list) > 0:
                 rss_card_dict[rss.title] = card_list
                 with db.atomic():
                     History.bulk_create(rss_history_list, batch_size=10)
 
         return rss_card_dict
+
+    def parse_summary(self, summary):
+        return summary.replace(
+            '<br /><br /><span style="font-size: 12px; color: gray;">(Feed generated with <a href="http://fetchrss.com" target="_blank">FetchRSS</a>)</span>',
+            '')
 
     def is_today(self, entry):
         return dateparser.parse(entry['updated']).date() == datetime.today().date()
@@ -50,16 +75,31 @@ class RssRobot:
     def send_rss(self):
         rss_card_dict = self.parse_rss()
         for key in rss_card_dict:
-            self.robot.send_feed_card(rss_card_dict[key])
-    
+            for item in rss_card_dict[key]:
+                self.request_feishu_webhook(item)
+
+    def request_feishu_webhook(self, carditem):
+        data = webhook_template.replace('$TITLE_TEXT', self.json_encode(carditem.title))
+        data = data.replace('$SUMMARY', self.json_encode(carditem.summary))
+        data = data.replace('$LINK', self.json_encode(carditem.url))
+        data = data.replace('$BLOG_NAME', self.json_encode(carditem.blog_name))
+        r = requests.post(url=webhook_url, headers={'Content-Type': 'application/json'}, data=data.encode('utf8'))
+        print(r.content)
+
     def remove_old_history(self):
-        history_list = History.delete().where(History.publish_at < datetime.today().strftime("%Y-%m-%d"))
+        # history_list = History.delete().where(History.publish_at < datetime.today().strftime("%Y-%m-%d"))
+        history_list = History.delete().where(True)
         history_list.execute()
+
+    def json_encode(self, s):
+        return s.replace('\\', '\\\\').replace('"', '\\"')
 
 
 def send_rss():
     rss_bot = RssRobot()
     rss_bot.send_rss()
+    pass
+
 
 if __name__ == '__main__':
     send_rss()
